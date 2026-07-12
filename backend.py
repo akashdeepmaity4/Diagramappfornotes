0274ec48e9b3931f0736c3d8144bf12afa992f34
akashdeepmaity4 <akashdeepmaity96@gmail.com>
2026-07-12 20:54:55 +0530

    removed error

--- backend.py
+++ backend.py
@@ -12,7 +12,7 @@ def save_canvas():
     try:
         payload = request.get_json()
         if not payload:
-            return jsonify({"error": "No data received"}), 400
+            return jsonify({"error": "No data received"}), 400 #i have saved it as json to reduce padload size
 
         saved_path = save_payload_as_png(payload)
 
