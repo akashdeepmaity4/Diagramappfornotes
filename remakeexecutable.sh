c2a995aff2108cd590da52606382cace2af4f115
akashdeepmaity4 <akashdeepmaity66@gmail.com>
2026-07-31 22:08:44 +0530

    making .exe app

--- remakeexecutable.sh
+++ remakeexecutable.sh
@@ -2,4 +2,4 @@ cd D:\projects\diagramappfornotes
 rmdir /s build
 rmdir /s dist
 del *.spec
-pyinstaller --onefile --windowed --add-data "templates;templates" --add-data "static;static" --add-data "app;backend" --add-data "noteimg;noteimg" --hidden-import flask --hidden-import jinja2 --hidden-import app --hidden-import app.backend --paths . launcher.py
+pyinstaller --onefile --windowed --add-data "templates;templates" --add-data "static;static" --add-data "app;backend" --add-data "noteimg;noteimg" --hidden-import flask --hidden-import jinja2 --hidden-import app --hidden-import app.backend --pathsx . launcher.py
--- utilities/app/backend.py
+++ utilities/app/backend.py
@@ -1,7 +1,36 @@
+import sys
+import os
+import re
 from flask import Flask, render_template, request, jsonify
 from utilities.exporter import make_img
+from werkzeug.utils import secure_filename
 
-app = Flask(__name__)
+#making executable application file
+
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.dirname(os.path.abspath(__file__))
+        if os.path.basename(base_path) == 'app':
+            base_path = os.path.dirname(base_path)
+    return os.path.join(base_path, relative_path)
+
+app = Flask(__name__,
+            template_folder=resource_path('templates'),
+            static_folder=resource_path('static'))
+
+'''attention here: storage directory'''
+STORAGE_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)),'storage') #CHNAGE THIS LINE TO CHANGE STORAGE LOCATION
+
+if not os.path.exists(STORAGE_DIR):
+    os.makedirs(STORAGE_DIR)
+
+
+
+
+
+    #main functionality
 
 @app.route('/')
 def index():
