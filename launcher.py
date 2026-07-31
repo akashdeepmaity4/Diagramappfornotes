a7b42b03ac54d94fe26174c78d32862d8ead5cf7
akashdeepmaity4 <akashdeepmaity66@gmail.com>
2026-07-31 21:59:53 +0530

    initial commit

---
+++ launcher.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import webview
+
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
+try:
+    from app.backend import backend
+    print("Successfully imported app from app.app")
+except ImportError as e:
+    print(f"Import error: {e}")
+    print("Files in current directory:", os.listdir('.'))
+    sys.exit(1)
+else:
+    print("ERROR: check launcher.py")
+
+if __name__ == '__main__':
+    webview.create_window('Diagram App for Notes', app)
+    webview.start()
